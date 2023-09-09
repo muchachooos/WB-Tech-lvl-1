668a7c3830c0aecd0661e3a6ff476dae96de3155
muchachooos <odnorogg@bk.ru>
2023-09-09 16:43:36 +0300

    Add task05 solution2

---
+++ task05/solution2/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	// Создание канала для передачи данных.
+	dataChan := make(chan int)
+	// Создаём WaitGroup.
+	wg := sync.WaitGroup{}
+
+	// Запуск горутины для отправки данных в канал.
+	go func() {
+		// Бесконечно отправляем в канал случайное число.
+		for {
+			dataChan <- rand.Int()
+			time.Sleep(time.Second)
+		}
+	}()
+
+	// Увеличиваем счетчик горутин в WaitGroup.
+	wg.Add(1)
+
+	// Запускаем горутину для получения данных из канала и их обработки.
+	go func() {
+		waitChan := time.After(5 * time.Second) // Создаём канал в который поступит сообщение через 5 секунд.
+		for {
+			// Ожидаем данные из каналов waitChan и dataChan.
+			select {
+			case <-waitChan: // Если в waitChan есть данные (то есть прошло 5 секунд), то завершаем ф-цию.
+				fmt.Println("Timeout")
+				wg.Done()
+				return
+			case data := <-dataChan: // Если в dataChan есть данные, то выводим их в консоль.
+				fmt.Println("Received:", data)
+			}
+		}
+	}()
+
+	// Ждём пока все горутины завершат свою работу.
+	wg.Wait()
+	fmt.Println("End")
+}
